d64b3592c189870d80065e6ae7ddfd3cddded5f0
Hidetake Iwata <int128@gmail.com>
2016-12-06 10:02:05 +0900

    WIP: OAuth2

--- api-gateway/src/main/groovy/configuration/HelloClientConfiguration.groovy
+++ api-gateway/src/main/groovy/configuration/HelloClientConfiguration.groovy
@@ -1,13 +1,36 @@
 package configuration
 
+import feign.RequestInterceptor
 import feign.codec.ErrorDecoder
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2ClientContext
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails
+import org.springframework.security.oauth2.common.AuthenticationScheme
 
 @Configuration
 class HelloClientConfiguration {
     @Bean
     ErrorDecoder errorDecoder() {
         new ErrorResponseDecoder()
+    }
+
+    @Bean
+    RequestInterceptor oauth2FeignRequestInterceptor(OAuth2ClientContext oauth2ClientContext){
+        new OAuth2FeignRequestInterceptor(oauth2ClientContext, resource())
+    }
+
+    @Bean
+    OAuth2ProtectedResourceDetails resource() {
+        def details = new ResourceOwnerPasswordResourceDetails()
+        details.accessTokenUri = '/oauth/token'
+        details.clientAuthenticationScheme = AuthenticationScheme.header
+        details.clientId = 'client'
+        details.clientSecret = 'secret'
+        details.username = 'user'
+        details.password = '754727e4-750f-451b-ac14-f03a8532ddcd'
+        details
     }
 }
--- server/src/main/groovy/example/server/App.groovy
+++ server/src/main/groovy/example/server/App.groovy
@@ -2,7 +2,11 @@ package example.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 
+@EnableAuthorizationServer
+@EnableResourceServer
 @SpringBootApplication
 class App {
     static void main(String[] args) {
